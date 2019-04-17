8336835e4d29b121c4e8d8345959e68efaa5b19f
neehhaa <neha@xyz.com>
2019-04-17 17:36:37 +0530

    List Tuples Dictionary Set

--- Data Types.py
+++ Data Types.py
@@ -1,6 +1,8 @@
-a=10
-b="python"
-c=1.2
+a=[1,2,"python",1.2]
+b=(89,45,"Java",5.99)
+c={1:"Python",2:"Java",3:"C"}
+d={"Python","Java","C"}
 print(type(a))
 print(type(b))
 print(type(c))
+print(type(d))
