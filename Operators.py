90f4d713af3c990f15e78d109a661aa069fbec81
neehhaa <neha@xyz.com>
2019-04-17 19:28:34 +0530

    Logical Operators

--- Operators.py
+++ Operators.py
@@ -1,20 +1,10 @@
-a=4
-b=5
+a= True
+b = False
 
-print(bin(a))       #100
-print(bin(b))       #101
+print(a and b)      #if both are true,output is true
 
-print(bin(a&b))     #100 AND operator if 1 then 1
+print(a or b)       #if any is true,output is true
 
-print(bin(a|b))     #101 OR operrator if any is 1 output is 1
+print(not a)        #if true then false
 
-print(bin(a^b))     #1 XOR operatoor if both different then 1
-
-print(bin(~a))      #-101 Negation operator
-
-print(bin(~b))      #-110 Negation operator
-
-print(bin(a<<b))    #10000000 a moved left by number of bits present in b operand
-
-print(bin(a>>b))    #0 a moved right by number of bits present in b operand
-
+print(not b)        #if true then false
