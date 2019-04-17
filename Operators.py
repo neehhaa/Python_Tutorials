9eaf64734191ba048d50561ccb48c1712168b181
neehhaa <neha@xyz.com>
2019-04-17 18:20:19 +0530

    Comparison Operator

--- Operators.py
+++ Operators.py
@@ -1,16 +1,17 @@
-a=10
-b=9
+a,b,c=3,5,3
 
-print(a+b)    #Addition
+print(a==b)     #False
 
-print(a-b)    #Subtraction
+print(a==c)     #True
 
-print(a*b)    #Multiplication
+print(a!=b)     #True
 
-print(a/b)    #Division
+print(a>=b)     #False
 
-print(a%b)    #Remainder
+print(a<=b)     #True
 
-print(a**b)   #Exponent
+print(a>b)      #False
 
-print(a//b)   #Floor Division 
+print(a<b)      #True
+
+print(a>b>c)    #False
