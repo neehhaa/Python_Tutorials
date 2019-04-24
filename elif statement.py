335af2571e02d8aa7c744b8277b31516e0f8ff86
neehhaa <neha@xyz.com>
2019-04-24 15:21:27 +0530

    Number determination

---
+++ elif statement.py
@@ -0,0 +1,7 @@
+number = int(input("Enter a number :   "))
+if number<10:
+    print("Number is less than 10")
+elif number<=50:
+    print("Number is less than or equal to 50")
+elif number>50:
+    print("Number is mo1re than 50")
