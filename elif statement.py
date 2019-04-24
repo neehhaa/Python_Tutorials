eab94bb5994e0e96741dde2396a5a1e1c502d7d3
neehhaa <neha@xyz.com>
2019-04-24 15:52:13 +0530

    Exam Result

--- elif statement.py
+++ elif statement.py
@@ -1,7 +1,19 @@
-number = int(input("Enter a number :   "))
-if number<10:
-    print("Number is less than 10")
-elif number<=50:
-    print("Number is less than or equal to 50")
-elif number>50:
-    print("Number is mo1re than 50")
+marks = int(input("Enter the marks :  "))
+if marks>=80:
+    print("Congrat!You secured A+ grade")
+elif 70<marks<80:
+    print("Congrat!You secured A grade")
+elif 60<marks<=70:
+    print("Congrat!You secured B+ grade")
+elif 55<marks<=60:
+    print("Congrat!You secured B grade")
+elif 50<marks<=55:
+    print("Congrat!You secured C+ grade")
+elif 45<marks<=50:
+    print("Congrat!You secured C grade")
+elif 40<marks<=45:
+    print("Congrat!You secured D+ grade")
+elif 35<=marks<=40:
+    print("Congrat!You secured D grade")
+elif marks<35:
+    print("FAILED!You secured F grade")
