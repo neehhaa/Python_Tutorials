7eddb821d638dcdd7dee107a0491b23d38fc6a4e
neehhaa <neha@xyz.com>
2019-04-24 14:58:49 +0530

    Largest Number

--- if statement.py
+++ if statement.py
@@ -1,3 +1,9 @@
-number = int(input("Enter a number :  "))
-if number%2==0:
-    print("Number is even!!")
+a = int(input("Enter a number :  "))
+b = int(input("Enter a number :  "))
+c = int(input("Enter a number :  "))
+if a>b and a>c:
+    print(a,"is greatest!!")
+if b>a and b>c:
+    print(b,"is greatest!!")
+if c>a and c>b:
+    print(c,"is greatest!!")
