6cb03e17d6fbc84fcd3abad6b740aedc6eb14686
neehhaa <neha@xyz.com>
2019-08-06 00:59:53 +0530

    Continue and Pass Statement

---
+++ Continue.py
@@ -0,0 +1,11 @@
+i=1
+for i in range(0,10):
+    if i == 5:
+        continue
+    print(i)
+print("\n\n\n")
+for n in [1,2,3,4,5]:
+    if n==3:
+        pass
+        print("Pass when value is",n)
+    print(n)
